9c2fb22041ace8c01175f0983b3bc6cdb37a5403
Hilbnwal <103248931+Hilbnwal@users.noreply.github.com>
2022-09-07 20:00:54 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Linq;
+
+namespace Lab_4
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			async Task<string> Result()
+			{
+				return await Task.Run(() => Generate()
+			   .ContinueWith(task => Generates(task.Result))
+			   .ContinueWith(task => Sum(task.Result.Result))
+			   .ContinueWith(task => TransformToString(task.Result.Result))).Result;
+			}
+
+			async Task<int> Generate()
+			{
+				return new Random().Next(1, 25);
+			}
+
+			async Task<List<double>> Generates(int count)
+			{
+				Console.WriteLine(" Первый результат:");
+				Console.WriteLine(count);
+				var list = new List<double>();
+				for (int i = 0; i < count; i++)
+					list.Add(new Random().NextDouble());
+				return list;
+			}
+
+			async Task<double> Sum(List<double> numbers)
+			{
+				Console.WriteLine("\n Второй результат:");
+				foreach (double i in numbers)
+				{ 
+					Console.WriteLine($"{i:f3}"); 
+				}
+				return numbers.Sum();
+			}
+
+			async Task<string> TransformToString(double number)
+			{
+				Console.WriteLine("\n Третий результат:");
+				Console.WriteLine($"{number:f3}");
+				return "Строка " + number.ToString("f3");
+			}
+
+			var task = Result();
+			Console.WriteLine("\n Четвертый результат:");
+			Console.WriteLine($"{task.Result:f3}");
+		}
+	}
+}
